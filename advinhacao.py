03d597c8488e8326a477f9ad8801753bc8720d51
Henrique <rique10099@gmai.com>
2024-12-01 18:19:57 -0300

    correção "advinhacao.py"

--- advinhacao.py
+++ advinhacao.py
@@ -1,33 +1,34 @@
 
-import random
-from escolher_dificuldade import dificuldade
-numero_secreto = random.randrange(1, 101)
-total_de_tentativas = 0
-rodada = 1
+from escolher_dificuldade import Dificuldade
 
-print("Bem-vindo ao jogo de adivinhação!")
+def jogar():
+    dificuldade = Dificuldade()
+    dificuldade.escolher_dificuldade()
 
-dificuldade()
+    total_de_tentativas = 0
+    rodada = 1
 
-while rodada <= 5:
-    print(f"Tentativa {rodada} de 5")
-    chute = input("Digite o seu número: ")
-    print("Você digitou ", chute)
-   
+    while rodada <= 5:
+        print(f"Tentativa {rodada} de 5")
+        chute = int(input("Digite o seu número: "))
+        print("Você digitou ", chute)
+    
+        numero_secreto = dificuldade.get_numero_secreto()
+        acertou = numero_secreto == chute
+        maior = chute > numero_secreto
+        menor = chute < numero_secreto
 
-    acertou = numero_secreto == chute
-    maior = chute > numero_secreto
-    menor = chute < numero_secreto
+        if acertou:
+            print("Você acertou!")
+            break
+        else:
+            if maior:
+                print("Você errou! O seu chute foi maior que o número secreto.")
+            elif menor:
+                print("Você errou! O seu chute foi menor que o número secreto.")
+        rodada = rodada + 1
 
-    if acertou:
-        print("Você acertou!")
-        break
-    else:
-        if maior:
-            print("Você errou! O seu chute foi maior que o número secreto.")
-        elif menor:
-            print("Você errou! O seu chute foi menor que o número secreto.")
-    rodada = rodada + 1
+    print("Fim do jogo")
 
-print("Fim do jogo")
+jogar()
 
